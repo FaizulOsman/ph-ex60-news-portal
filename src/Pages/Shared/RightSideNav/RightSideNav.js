ccfe2dbd634da6f95ca64a6167fb8a0866e9e6ce
Faizul Osman <faizulosmanrabby@gmail.com>
2022-10-18 18:45:50 +0600

    first commit

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, ButtonGroup, ListGroup } from "react-bootstrap";
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaTwitch,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+import BrandCarousel from "../BrandCarousel/BrandCarousel";
+
+const RightSideNav = () => {
+  return (
+    <div>
+      <ButtonGroup className="mb-4" vertical>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          <FaGithub /> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div>
+        <h5>Find Us On</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> Whatsapp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch /> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">Terms and conditions</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel />
+      </div>
+    </div>
+  );
+};
+
+export default RightSideNav;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhYogOKEMVKNFq3Dwd7C5Cdo9gqRHWvjg",
+  authDomain: "news-portal-15456.firebaseapp.com",
+  projectId: "news-portal-15456",
+  storageBucket: "news-portal-15456.appspot.com",
+  messagingSenderId: "907162968110",
+  appId: "1:907162968110:web:3873e22d897feea2537fbf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
